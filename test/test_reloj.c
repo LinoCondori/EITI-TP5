3d75bdc533a33c95f065cacb441e4345795e83c9
Condori Lino <condori.lino.f@gmail.com>
2022-12-07 14:47:18 -0300

    primera prueba que pasa

---
+++ test/test_reloj.c
@@ -0,0 +1,27 @@
+#include "unity.h"
+#include "clock.h"
+/*
+1) Configurar la libreria, consultar la hora y tiene que invalida.
+2) Configurar la libreria, ajustar la hora (con valores correctos), consultar la hora y tiene que ser valida.
+3) Configurar la libreria, ajustar la hora (con valores incorrectos) tiene que devolver un error y seguir siendo invalida.
+4) Simular el paso de n ciclos de reloj, consultar la hora y verificar que avanzo un segundo.
+5) Simular el paso de 10*n ciclos de reloj, consultar la hora y verificar que avanzo 10 segundos y varios igual.
+6) Configurar la hora de la alarma (con valores correctos) y revisar si la guarda.
+7) Configurar la hora de la alarma (con valores incorrectos) y revisar si la rechaza.
+8) Configurar la hora de la alarma (con valores correctos) y revisar si queda activada.
+9) Si la alarma esta activa y la desactivo, queda desactiva, pero no cambia la hora.
+10) Si la alarma esta desactiva y la activo, queda activa, pero no cambia la hora.
+11) Si la alarma esta activa, y la hora del reloj coincide con la hora de la alarma, entonces suena.
+12) Si la alarma se activo y la pospongo n minutos, vuelve a sonar en n minutos.
+13) Si la alarma se activo y la cancelo, vuelve a sonar en al dia siguiente.
+14) 
+*/
+
+//1) Configurar la libreria, consultar la hora y tiene que invalida.
+void test_hora_inicial_invalida(void) {
+    static const uint8_t ESPERADO[] = {0, 0, 0, 0, 0, 0, 0};
+    uint8_t hora[6];
+    clock_t reloj = ClockCreate(5);
+    TEST_ASSERT_FALSE(ClockGetTime(reloj, hora, 6));
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO, hora, 6);
+}
